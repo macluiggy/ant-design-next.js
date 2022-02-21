d676889b471ef44a9a09bc765e00cbaf68bf59b5
luiggy macias <ferrinluiggy@gmail.com>
2022-02-21 11:24:37 -0500

    vvv

--- src/pages/input.tsx
+++ src/pages/input.tsx
@@ -2,7 +2,10 @@ import { Container } from "../components/Container";
 import { Input } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { useState } from "react";
-
+// import Password from "antd/lib/input/Password";
+import { Select } from "antd";
+import { Option } from "antd/lib/mentions";
+const { Password } = Input;
 const input = () => {
   const [inputText, setInputText] = useState({
     input: "",
@@ -13,6 +16,21 @@ const input = () => {
     setInputText({ ...inputText, [name]: value });
     console.log(inputText);
   };
+
+  const selectInicio = (
+    <Select defaultValue="1">
+      <Option value="1">http://</Option>
+      <Option value="2">https://</Option>
+    </Select>
+  );
+  const selectFinal = (
+    <Select defaultValue="1">
+      <Option value="1">.com</Option>
+      <Option value="2">.net</Option>
+      <Option value="3">.org</Option>
+      <Option value="4">.edu</Option>
+    </Select>
+  );
   return (
     <Container>
       <Input placeholder="Basic usage" size="large" prefix={<UserOutlined />} />
@@ -23,6 +41,14 @@ const input = () => {
         size="small"
         onChange={handleChange}
         name="input"
+      />
+      <Password placeholder="peror" bordered={false} maxLength={10} />
+      <Input
+        placeholder="Basic usage"
+        defaultValue={"Mi sitio Web"}
+        size="small"
+        addonBefore={selectInicio}
+        addonAfter={selectFinal}
       />
     </Container>
   );
